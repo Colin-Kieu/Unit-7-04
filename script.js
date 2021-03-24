d447361247014a64019a3a5df8c4aa0c66267f42
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-24 19:34:59 +0000

    linter

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function action () {
   age = parseInt(age)
   day = document.getElementById('input2').value
   // this will convert the input into an integer so our computer can read it, .value will store our input
-  if ((day === 'tuesday' || day == 'thursday') || (age >= 12 && age <= 21)) {
+  if ((day === 'tuesday' || day === 'thursday') || (age >= 12 && age <= 21)) {
     document.getElementById('answer').innerHTML = 'You are eligilbe for student pricing!'
     // if the day is tuesday or thursday, everyone can get student pricing also, if they are between the age of 12 and 21 they can get student pricing
   } else {
